a5b8a8d619d0cd342d6bb792160177f9c37872d7
Jungley Yeh <yejing@live.cn>
2023-03-11 18:59:47 +0800

    feat: deploy to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
 import { defineConfig } from 'astro/config';
-
 import node from '@astrojs/node';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
+  // if 使用 docker, 请将 adapter 切换到 node
+  // adapter: node({
+  //   mode: 'standalone'
+  // }),
+  adapter: vercel({
+    analytics: true
   }),
   integrations: [react(), tailwind()],
   markdown: {
     // Example: Switch to use prism for syntax highlighting in Markdown
-    syntaxHighlight: 'prism',
+    syntaxHighlight: 'prism'
   }
 });
