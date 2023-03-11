4dfa3a0735b43d0f7d7aa802c2de57f3e5fb7266
Jungley Yeh <yejing@live.cn>
2023-03-11 11:08:28 +0800

    intergration with react\tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind()]
+});
