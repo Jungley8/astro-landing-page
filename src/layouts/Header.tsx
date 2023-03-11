d59d4f8785d0b497ece7f6830821c61934b131d5
Jungley Yeh <yejing@live.cn>
2023-03-11 19:20:51 +0800

    fix: Correct link error in mobile navigation

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -238,22 +238,28 @@ export default function Header() {
                   )}
                 </Disclosure>
                 <a
-                  href="#"
+                  href="/#newsletter"
                   className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
-                  Features
+                  Newsletter
                 </a>
                 <a
-                  href="#"
+                  href="/price"
                   className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
-                  Marketplace
+                  Price
                 </a>
                 <a
-                  href="#"
+                  href="/contact"
                   className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
-                  Company
+                  Contact
+                </a>
+                <a
+                  href="/blog"
+                  className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Blog
                 </a>
               </div>
               <div className="py-6">
