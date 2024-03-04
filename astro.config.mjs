75b131638ce31e6bc17f4dc3d094c8cff09cd114
Jungley Yeh <yejing@live.cn>
2024-03-04 22:00:51 +0800

    upgrade to astro 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
